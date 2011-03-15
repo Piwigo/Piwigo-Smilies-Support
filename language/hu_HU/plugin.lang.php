eff9203542ca7380d1a5c873a06ddbb50b265612
samli <samli@piwigo.org>
2011-03-15 14:16:05 +0000

    [extensions] [SmiliesSupport] modified HU language
    git-svn-id: http://piwigo.org/svn/extensions/SmiliesSupport@9701 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/hu_HU/plugin.lang.php
+++ language/hu_HU/plugin.lang.php
@@ -2,7 +2,7 @@
 
 $lang['smilies_dir'] = "Hangulatjelek elérési útja";
 $lang['nb_columns'] = "Oszlopok száma";
-$lang['representant'] = "Képviselo";
-$lang['All Smilies'] = "All Smilies";
+$lang['representant'] = "Képviselő";
+$lang['All Smilies'] = "Összes hangulatjel";
 
 ?>
