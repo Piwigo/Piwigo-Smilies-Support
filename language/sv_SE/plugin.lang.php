cf83035ae259c1873857074e77908ccb14822989
jonas <jonas@piwigo.org>
2011-03-21 22:45:52 +0000

    Just swedish by jonas
    git-svn-id: http://piwigo.org/svn/extensions/SmiliesSupport@9816 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sv_SE/plugin.lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['smilies_dir'] = "Smileys' Mappar";
+$lang['nb_columns'] = "Nb. Kolumer";
+$lang['representant'] = "Representative";
+$lang['All Smilies'] = "Alla Smilies";
+$lang['smilies_file_help'] = "Som standard när en användare klickar på en smiley, tillägger Smilies Stödja ett sökord som matchar namnet på filen (smile.png -> :smile:).<br>
+	Men här kan du konfigurera andra strängar som ska ersättas. Varje smilies mapp har sin egen <i>smilies.txt</i> fil.";
+
+?>
