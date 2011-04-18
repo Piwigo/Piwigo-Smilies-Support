fb1ff865651802dbe03cbcfeb73d9532ca7f6fd0
ddtddt <ddtddt@piwigo.org>
2011-04-18 07:52:04 +0000

    [extensions] - SmiliesSupport - update SK Thanks to dodo
    git-svn-id: http://piwigo.org/svn/extensions/SmiliesSupport@10462 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/sk_SK/plugin.lang.php
+++ language/sk_SK/plugin.lang.php
@@ -4,7 +4,7 @@ $lang['smilies_dir'] = "Adresár smajlíkov";
 $lang['nb_columns'] = "Počet stĺpcov";
 $lang['representant'] = "Zástupca";
 $lang['All Smilies'] = "Všetky smajlíky";
-$lang['smilies_file_help'] = "By default when a user clicks on a smiley, Smilies Support adds a keyword matching the name of the file (smile.png -> :smile:).<br>
-	But here you can configure other strings to be replaced. Each smilies folder has its own <i>smilies.txt</i> file.";
+$lang['smilies_file_help'] = 'Predvolene, ak používateľ klikne na smajlíka, podpora smajlíkov pridá kľúčové slovo spárované s menom súboru (smile.png -> :smile:).<br>
+    Ale môžete konfigurovať iné reťazce na prepísanie. Každý adresár smajlíka má vlastný <i>smilies.txt</i> súbor.';
 
 ?>
