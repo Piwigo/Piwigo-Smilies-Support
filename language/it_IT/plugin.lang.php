fe0a4fd2aaee0a303ce46d04db03da9fdc28ea1d
riopwg <riopwg@piwigo.org>
2011-04-26 08:19:08 +0000

    Update language IT
    git-svn-id: http://piwigo.org/svn/extensions/SmiliesSupport@10622 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/it_IT/plugin.lang.php
+++ language/it_IT/plugin.lang.php
@@ -1,10 +1,10 @@
 <?php
 
-$lang['smilies_dir'] = "Cartella degli smileys";
-$lang['nb_columns'] = "Num. di colonne";
-$lang['representant'] = "Rappresentante";
-$lang['All Smilies'] = "Tutti gli smiley";
-$lang['smilies_file_help'] = "By default when a user clicks on a smiley, Smilies Support adds a keyword matching the name of the file (smile.png -> :smile:).<br>
-	But here you can configure other strings to be replaced. Each smilies folder has its own <i>smilies.txt</i> file.";
+$lang['smilies_dir'] = 'Cartella degli smileys';
+$lang['nb_columns'] = 'Num. di colonne';
+$lang['representant'] = 'Rappresentante';
+$lang['All Smilies'] = 'Tutti gli smiley';
+$lang['smilies_file_help'] = 'Di default, quando un utente clicca su un smiley, SmiliesSupport aggiunge una keyword corrispondente al nome del fine (smile.png -> :smile:).<br>
+	Ma potete configurare qui altre serie di caratteri che saranno rimpiazzate. Ogni cartella di smileys ha il suo file <i>smilies.txt</i>';
 
 ?>
