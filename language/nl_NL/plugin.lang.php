95a33e6014ca54f36492b99a0083522d32545eb4
ddtddt <ddtddt@piwigo.org>
2011-04-29 20:36:03 +0000

    [extensions] - SmiliesSupport - add nl_NL thanks to Mattias
    git-svn-id: http://piwigo.org/svn/extensions/SmiliesSupport@10695 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['smilies_dir'] = 'Smiley\'s map';
+$lang['nb_columns'] = 'Nb. kolommen';
+$lang['representant'] = 'Representatieve';
+$lang['All Smilies'] = 'Alle Smileys';
+$lang['smilies_file_help'] = 'Als een gebruiker op een smiley klikt, voegt de Smilies Support standaard een trefwoord toe die overeenkomt met de naam van het bestand (smile.png -> :smile:). <br>
+Maar hier kan je andere trefwoorden instellen. Elke smiley map heeft zijn eigen <i>smilies.txt</i> bestand.';
+?>
