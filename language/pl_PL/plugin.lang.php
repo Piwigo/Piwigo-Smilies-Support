10282e26606eb51317262964c7a3294d3227cde2
ddtddt <ddtddt@piwigo.org>
2011-05-06 16:23:52 +0000

    [extensions] - SmiliesSupport - add PL thanks to Ortolan
    git-svn-id: http://piwigo.org/svn/extensions/SmiliesSupport@10794 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/pl_PL/plugin.lang.php
+++ language/pl_PL/plugin.lang.php
@@ -1,10 +1,10 @@
 <?php
 
-$lang['smilies_dir'] = "Folder z emotikonami";
-/*TODO explain*/$lang['nb_columns'] = "Nb. columns";
-$lang['representant'] = "Reprezentant";
-$lang['All Smilies'] = "Wszystkie emotikony";
-$lang['smilies_file_help'] = "Domyślnie, gdy użytkownik kliknie w emotikon, Smilies Support doda słowo kluczowe odpowiadające nazwie danego pliku z emotikonem (smile.png -> :smile:). <br>
-    Jednak w tym miejscu możesz także skonfigurować inne ciągi znaków. Każdy folder z emotikonami ma swój własny <i>smilies.txt</i> plik ";
+$lang['smilies_dir'] = 'Folder z emotikonami';
+$lang['nb_columns'] = 'Liczba kolumn'; 
+$lang['representant'] = 'Reprezentant';
+$lang['All Smilies'] = 'Wszystkie emotikony';
+$lang['smilies_file_help'] = 'Domyślnie, gdy użytkownik kliknie w emotikon, Smilies Support doda słowo kluczowe odpowiadające nazwie danego pliku z emotikonem (smile.png -> :smile:). <br>
+    Jednak w tym miejscu możesz także skonfigurować inne ciągi znaków. Każdy folder z emotikonami ma swój własny <i>smilies.txt</i> plik ';
 
 ?>
