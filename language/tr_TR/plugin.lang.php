9220422487554f3f41203cf77946f9dd8a3b892c
Nakre <Nakre@piwigo.org>
2011-06-22 12:32:27 +0000

    Updated Turkish Translates
    git-svn-id: http://piwigo.org/svn/extensions/SmiliesSupport@11478 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/tr_TR/plugin.lang.php
+++ language/tr_TR/plugin.lang.php
@@ -2,9 +2,9 @@
 
 $lang['smilies_dir'] = "İfadeler' dosya";
 $lang['nb_columns'] = "Numara Sütunü";
-$lang['representant'] = "Temsilcisi";
+$lang['representant'] = "Temsilci";
 $lang['All Smilies'] = "Tüm İfadeler";
-$lang['smilies_file_help'] = "By default when a user clicks on a smiley, Smilies Support adds a keyword matching the name of the file (smile.png -> :smile:).<br>
-  But here you can configure other strings to be replaced. Each smilies folder has its own <i>smilies.txt</i> file.";
+$lang['smilies_file_help'] = "Kullanıcı bir ifadeye tıkladığı zaman, Smilies Support eklentisi ifade ile eşleşen bir anahtar kelime ekler örnek (smile.png -> :smile:).<br>
+  Burada diğer anahtar kelimeleri düzenleyebilirsiniz. Her gülümseme klasörü kendi <i>smilies.txt</i> dosyasına sahiptir.";
 
 ?>
