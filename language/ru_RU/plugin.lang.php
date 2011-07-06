83aa4915dcb493f4ba71db70d63722c8084967a6
ddtddt <ddtddt@piwigo.org>
2011-07-06 06:49:33 +0000

    [extensions] - SmiliesSupport - update Ru (Русский - Russian) thanks to yuragal
    git-svn-id: http://piwigo.org/svn/extensions/SmiliesSupport@11642 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/ru_RU/plugin.lang.php
+++ language/ru_RU/plugin.lang.php
@@ -1,10 +1,9 @@
 <?php
 
-$lang['smilies_dir'] = "Smileys' folder";
-$lang['nb_columns'] = "Nb. columns";
-$lang['representant'] = "Representative";
-$lang['All Smilies'] = "All Smilies";
-$lang['smilies_file_help'] = "By default when a user clicks on a smiley, Smilies Support adds a keyword matching the name of the file (smile.png -> :smile:).<br>
-  But here you can configure other strings to be replaced. Each smilies folder has its own <i>smilies.txt</i> file.";
+$lang['smilies_dir'] = 'Папка со смайликами';
+$lang['nb_columns'] = 'Количество';
+$lang['representant'] = 'Представитель';
+$lang['All Smilies'] = 'Все смайлики';
+$lang['smilies_file_help'] = 'По умолчанию, когда пользователь кликает на смайлик, плагин добавляет коючевое слово, соотретствующее имени файла со смайликом (например, smile.png -> :smile:).<br> С помощью плагина вы можете настроить автозамену. В каждой папке со смайликами находится файл <i>smilies.txt</i>.';
 
 ?>
