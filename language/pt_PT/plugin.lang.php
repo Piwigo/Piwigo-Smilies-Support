98d4be44deeafdb84af6c57f51771ea10b51ba1a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-04-25 11:49:13 +0000

    [smiliessupport] Add pt_PT, thanks to : klito
    git-svn-id: http://piwigo.org/svn/extensions/SmiliesSupport@14393 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pt_PT/plugin.lang.php
@@ -0,0 +1,29 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['All Smilies'] = 'Todos os icons smille';
+$lang['Nb. columns'] = 'Colunas nb.';
+$lang['Representative'] = 'Representativo';
+$lang['Smileys\' folder'] = 'Directório dos icons smille';
+$lang['smilies_file_help'] = 'Por defeito, quando um utilizador clica num icon smille,o suporte dos Smilles adiciona uma palavra chave coorrespondente ao nome do smille pretendido (smille.png->: smille :)<br>
+Mas aqui você pode configurar sequências para substituição. Cada pasta tem o seu próprio arquivo <i>smilies.txt</i>.';
+?>
