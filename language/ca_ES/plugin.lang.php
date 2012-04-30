f1229b2be007e526d1af6c5b2b85b34c93dbf170
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-04-30 11:57:40 +0000

    [SmiliesSupport] Update Catalan [CA], thanks to : AlbertParera
    git-svn-id: http://piwigo.org/svn/extensions/SmiliesSupport@14586 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ca_ES/plugin.lang.php
@@ -0,0 +1,29 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['All Smilies'] = 'Tots els emoticons';
+$lang['Nb. columns'] = 'Mb. Columnes';
+$lang['Representative'] = 'Representatiu';
+$lang['Smileys\' folder'] = 'Carpeta d\'emoticons';
+$lang['smilies_file_help'] = 'Per defecte, quan un usuari fa clic a una emoticona, el Suport d\'Emoticones afegeix una paraula clau que coincideixi amb el nom de l\'arxiu (smile.png -> :smile:).<br>
+Però aquí es pot ajustar el reemplaçament de diverses cadenes. Cada carpeta d\'emoticones té el seu arxiu propi <i>smilies.txt</i>.';
+?>
