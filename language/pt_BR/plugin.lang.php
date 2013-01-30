68fd31d07f0483a1d3cfff3d3be82439066c3257
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-30 12:46:32 +0000

    [smiliessupport] Add pt_BR, thanks to : flaviove
    git-svn-id: http://piwigo.org/svn/extensions/SmiliesSupport@20463 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pt_BR/plugin.lang.php
@@ -0,0 +1,29 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['All Smilies'] = 'Todos os Smilies';
+$lang['Nb. columns'] = 'Nu. colunas';
+$lang['Representative'] = 'Representante';
+$lang['Smileys\' folder'] = 'Pasta de smileys';
+$lang['smilies_file_help'] = 'Por padrão, quando um usuário clica em um smiley, Suporte Smilies acrescenta uma palavra-chave correspondente ao nome do arquivo (smile.png ->:smile:).<br>
+Mas aqui você pode configurar outras strings para serem substituídas. Cada pasta de arquivo tem seus próprios arquivos <i>smilies.txt</i>.';
+?>
