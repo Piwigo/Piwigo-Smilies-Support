0f0b01afbb4a371de08f6f063209987d16d95a8c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-05-18 07:08:01 +0000

    [smiliessupport] Add bg_BG, thanks to : turnokopa
    git-svn-id: http://piwigo.org/svn/extensions/SmiliesSupport@22731 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/bg_BG/plugin.lang.php
@@ -0,0 +1,29 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['smilies_file_help'] = 'По подразбиране когато потребител натисне на усмивка, плъгина добавя съответната дума отговаряща на този файл (smile.png -> :smile:)<br> 
+ Но тук вие може да добавяте или променяте стрингове. Всяка усмивка има собствен <i>smilies.txt</i> файл.';
+$lang['Smileys\' folder'] = 'Папка със усмивки';
+$lang['Representative'] = 'Представен';
+$lang['Nb. columns'] = 'Брой колони';
+$lang['All Smilies'] = 'Добави усмивки';
+?>
