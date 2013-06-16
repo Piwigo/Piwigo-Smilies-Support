40b1882c9a6a8f6836c8ea28c789b0acb5c1bf0d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-06-16 16:14:28 +0000

    [smiliessupport] Add zh_HK, thanks to : hkdigit
    git-svn-id: http://piwigo.org/svn/extensions/SmiliesSupport@23257 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_HK/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Columns'] = '列';
+$lang['If you change current set you will lost every shortcuts changes.'] = '如果你改變當前的設定，你將失去了所有更改了捷徑。';
+$lang['Reset defaults'] = '重設預設值';
+$lang['Shortcuts'] = '捷徑';
+$lang['Smilies'] = '表情符號';
+$lang['Smilies set'] = '表情集';
+$lang['Type in a new shortcut'] = '輸入新的捷徑';
+?>
