05d7507be883eab950d270479d74d4168997c488
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-06-16 16:13:49 +0000

    [smiliessupport] Update es_ES, thanks to : jpr928
    git-svn-id: http://piwigo.org/svn/extensions/SmiliesSupport@23253 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/es_ES/plugin.lang.php
@@ -0,0 +1,31 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Columns'] = 'Columnas';
+$lang['Edit shortcuts'] = 'Editar accesos directos';
+$lang['If you change current set you will lost every shortcuts changes.'] = 'Si cambia la configuración actual se perderá los cambios de los accesos directos.';
+$lang['Reset defaults'] = 'Restablecer valores predeterminados';
+$lang['Shortcuts'] = 'Accesos directos';
+$lang['Smilies'] = 'Emoticonos';
+$lang['Smilies set'] = 'Establecer emoticonos';
+$lang['Type in a new shortcut'] = 'Introduzca un nuevo acceso directo';
+?>
