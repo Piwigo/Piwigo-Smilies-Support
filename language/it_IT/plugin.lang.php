97e7866c5746750d1f1a1a3af405ca1176898344
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-06-16 20:00:13 +0000

    [smiliessupport] Update it_IT, thanks to : Ericnet
    git-svn-id: http://piwigo.org/svn/extensions/SmiliesSupport@23274 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/it_IT/plugin.lang.php
@@ -0,0 +1,31 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Type in a new shortcut'] = 'Digitare un nuovo collegamento';
+$lang['Smilies set'] = 'Set faccine';
+$lang['Smilies'] = 'Faccine';
+$lang['Shortcuts'] = 'Scorciatoie';
+$lang['Reset defaults'] = 'Ripristina impostazioni predefinite';
+$lang['If you change current set you will lost every shortcuts changes.'] = 'Se si modifica il set corrente sarà persa ogni modifica alle scorciatoie.';
+$lang['Edit shortcuts'] = 'Modifica scorciatoie';
+$lang['Columns'] = 'Colonne';
+?>
