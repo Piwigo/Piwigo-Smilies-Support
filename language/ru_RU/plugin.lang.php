58d07f2a41d5f87babe9a75b5423ad55a0e9d194
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-06-16 16:14:37 +0000

    [smiliessupport] Update ru_RU, thanks to : Konve
    git-svn-id: http://piwigo.org/svn/extensions/SmiliesSupport@23258 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ru_RU/plugin.lang.php
@@ -0,0 +1,29 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Columns'] = 'Столбцы';
+$lang['If you change current set you will lost every shortcuts changes.'] = 'Если Вы смените текущий набор, то будут потеряны все предыдущие изменения ярлыков.';
+$lang['Reset defaults'] = 'Сбросить настройки на "по умолчанию"';
+$lang['Shortcuts'] = 'Ярлыки';
+$lang['Smilies'] = 'Смайлики';
+$lang['Smilies set'] = 'Набор смайликов';
+?>
