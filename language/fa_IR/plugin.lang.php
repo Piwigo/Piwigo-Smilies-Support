69403ec0a47ba5678a6ffe3e5bf2555f17030a85
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-06-17 04:37:50 +0000

    [smiliessupport] Add fa_IR, thanks to : mnsb13600
    git-svn-id: http://piwigo.org/svn/extensions/SmiliesSupport@23282 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/fa_IR/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Columns'] = 'ستون';
+$lang['Edit shortcuts'] = 'ویرایش کلید های میانبر';
+$lang['Reset defaults'] = 'تنظیم مجدد پیش فرض';
+$lang['Shortcuts'] = 'کلید های میانبر';
+$lang['Smilies'] = 'شکلک ها';
+$lang['Smilies set'] = 'مجموعه شکلک ها';
+$lang['Type in a new shortcut'] = 'تایپ در یک میانبر جدید';
+?>
