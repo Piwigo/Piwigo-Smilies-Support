d4c965eedc5f8b9dff555faed32c6228a933ab25
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-06-17 21:34:57 +0000

    [smiliessupport] Update de_DE, thanks to : Joergen
    git-svn-id: http://piwigo.org/svn/extensions/SmiliesSupport@23312 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/de_DE/plugin.lang.php
@@ -0,0 +1,31 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Columns'] = 'Spalten';
+$lang['Edit shortcuts'] = 'Verknüpfung editieren';
+$lang['If you change current set you will lost every shortcuts changes.'] = 'Wenn sie das derzeitige Set ändern verlieren sie alle Änderungen an den Verknüpfungen.';
+$lang['Reset defaults'] = 'Auf Standard Werte zurück setzen';
+$lang['Shortcuts'] = 'Verknüpfung';
+$lang['Smilies'] = 'Smilies';
+$lang['Smilies set'] = 'Smilies Set';
+$lang['Type in a new shortcut'] = 'Eine neue Verknüpfung eingeben';
+?>
