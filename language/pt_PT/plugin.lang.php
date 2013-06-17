beaa7360e02fd3f8a4a225171463cb2b8e3a107e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-06-17 21:34:24 +0000

    [smiliessupport] Update pt_PT, thanks to : ANO
    git-svn-id: http://piwigo.org/svn/extensions/SmiliesSupport@23309 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pt_PT/plugin.lang.php
@@ -0,0 +1,31 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Smilies'] = 'Smiles';
+$lang['Smilies set'] = 'Definir smiles';
+$lang['Columns'] = 'Colunas';
+$lang['Edit shortcuts'] = 'Editar atalhos';
+$lang['If you change current set you will lost every shortcuts changes.'] = 'Se alterar as definições atuais, perderá as alterações de atalhos';
+$lang['Reset defaults'] = 'Redefinir padrões';
+$lang['Shortcuts'] = 'Atalhos';
+$lang['Type in a new shortcut'] = 'Escrever novo atalho';
+?>
