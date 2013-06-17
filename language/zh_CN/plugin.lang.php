07d483b6d8a4cb710a059c516cb50f4a74b51f03
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-06-17 21:32:39 +0000

    [smiliessupport] Update zh_CN, thanks to : winson
    git-svn-id: http://piwigo.org/svn/extensions/SmiliesSupport@23297 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_CN/plugin.lang.php
@@ -0,0 +1,31 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Columns'] = '列';
+$lang['Edit shortcuts'] = '编辑快捷键';
+$lang['If you change current set you will lost every shortcuts changes.'] = '如果你修改当前组合，你将丢失所有快捷键的改动';
+$lang['Reset defaults'] = '重置回默认';
+$lang['Shortcuts'] = '快捷键';
+$lang['Smilies'] = '表情符号';
+$lang['Smilies set'] = '表情符号组';
+$lang['Type in a new shortcut'] = '添加一个新的快捷键';
+?>
