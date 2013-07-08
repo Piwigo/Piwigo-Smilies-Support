ecaa3bf6f31ae8326890501cfbd0d717aec34aac
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-07-08 11:29:08 +0000

    [smiliessupport] Add et_EE, thanks to : Eggy
    git-svn-id: http://piwigo.org/svn/extensions/SmiliesSupport@23877 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/et_EE/plugin.lang.php
@@ -0,0 +1,31 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Columns'] = 'Veerud';
+$lang['Edit shortcuts'] = 'Redigeeri otseteed';
+$lang['If you change current set you will lost every shortcuts changes.'] = 'Kui sa muudad antud kogumit kaotad kõik otseteede muudatused';
+$lang['Reset defaults'] = 'Algseadista väärtused';
+$lang['Shortcuts'] = 'Otseteed';
+$lang['Smilies'] = 'Emotikoonid';
+$lang['Smilies set'] = 'Emotikoonide kogum';
+$lang['Type in a new shortcut'] = 'Trüki uus otsetee';
+?>
