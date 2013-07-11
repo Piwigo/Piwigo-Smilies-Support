24ceb4b48b39d5e92f11f04b0cb411c1bbc7ae1e
ddtddt <ddtddt@piwigo.org>
2013-07-11 06:04:59 +0000

    [extensions] - SmiliesSupport -Add zh_TW - 中文 (繁體) - Chinese traditional Thanks to chouyh
    git-svn-id: http://piwigo.org/svn/extensions/SmiliesSupport@23907 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_TW/plugin.lang.php
@@ -0,0 +1,10 @@
+<?php
+$lang['Smilies set'] = '表情符號設定';
+$lang['Shortcuts'] = '快捷鍵';
+$lang['Columns'] = '欄';
+$lang['Smilies'] = '表情符號';
+$lang['Edit shortcuts'] = '編輯快捷鍵';
+$lang['Reset defaults'] = '回到預設';
+$lang['If you change current set you will lost every shortcut schanges.'] = '如果你改變 了當前的設置，你會失去所有快捷鍵的改變.';
+$lang['Type in a new shortcut'] = '新的快捷鍵型式';
+?>
