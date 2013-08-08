2601cbd21c97346e2085a7311142c39e76dc531a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-08-08 16:47:36 +0000

    [smiliessupport] Update vi_VN, thanks to : dummy
    git-svn-id: http://piwigo.org/svn/extensions/SmiliesSupport@24146 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/vi_VN/plugin.lang.php
@@ -0,0 +1,31 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Columns'] = 'Cột';
+$lang['Edit shortcuts'] = 'Sửa shortcut';
+$lang['If you change current set you will lost every shortcuts changes.'] = 'Nếu thay đổi bộ hiện tại, tất cả các thay đổi shortcut sẽ bị mất';
+$lang['Reset defaults'] = 'Đặt thiết lập mặc định';
+$lang['Shortcuts'] = 'Shortcut';
+$lang['Smilies'] = 'Các biểu tượng vui';
+$lang['Smilies set'] = 'Bộ các biểu tượng vui';
+$lang['Type in a new shortcut'] = 'Nhập một shortcut mới';
+?>
