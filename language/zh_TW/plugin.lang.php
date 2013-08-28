a413344df57bfff3417dba5a95995d6ef9779565
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-08-28 20:35:40 +0000

    [smiliessupport] Update zh_TW, thanks to : azq1231
    git-svn-id: http://piwigo.org/svn/extensions/SmiliesSupport@24282 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/zh_TW/plugin.lang.php
+++ language/zh_TW/plugin.lang.php
@@ -7,4 +7,5 @@ $lang['Edit shortcuts'] = '編輯快捷鍵';
 $lang['Reset defaults'] = '回到預設';
 $lang['If you change current set you will lost every shortcut schanges.'] = '如果你改變 了當前的設置，你會失去所有快捷鍵的改變.';
 $lang['Type in a new shortcut'] = '新的快捷鍵型式';
+$lang['If you change current set you will lost every shortcuts changes.'] = '如果你修改當前組合，你將丟失所有快捷鍵的改動';
 ?>
