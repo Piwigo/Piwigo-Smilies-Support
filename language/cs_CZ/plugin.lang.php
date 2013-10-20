c1f5d3ef81bba34708a8ae9512fd62eca4ed64ee
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-10-20 20:24:00 +0000

    [smiliessupport] Update cs_CZ, thanks to : coudy
    git-svn-id: http://piwigo.org/svn/extensions/SmiliesSupport@25044 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/cs_CZ/plugin.lang.php
@@ -0,0 +1,31 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Columns'] = 'Sloupce';
+$lang['Edit shortcuts'] = 'Upravit zkratky';
+$lang['If you change current set you will lost every shortcuts changes.'] = 'Budou ztraceny všechny změny zkratek ,jestli se teď změní současná sada.';
+$lang['Reset defaults'] = 'Obnovit původní nastavení';
+$lang['Shortcuts'] = 'Zkratky';
+$lang['Smilies'] = 'Smajlíci';
+$lang['Smilies set'] = 'Sada smajlíků';
+$lang['Type in a new shortcut'] = 'Vložit novou zkratku';
+?>
