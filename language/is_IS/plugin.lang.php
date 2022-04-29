6e58ff22a5292a939108aa0868ebadc687f1cb5d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-04-29 06:57:47 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Columns'] = 'Dálkar';
+$lang['Edit shortcuts'] = 'Breyta flýtileiðum';
+$lang['If you change current set you will lost every shortcuts changes.'] = 'Ef þú breytir núverandi setti muntu tapa öllum breytingum á flýtivísunum.';
+$lang['Reset defaults'] = 'Endurstilla sjálfgefna stillingar';
+$lang['Shortcuts'] = 'Flýtivísar';
+$lang['Smilies'] = 'Broskarlar';
+$lang['Smilies set'] = 'Broskarla sett';
+$lang['Type in a new shortcut'] = 'Sláðu inn nýjan flýtivísi';
