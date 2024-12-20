d48ff8ed9a016afb866ec0733d3bc70466cdc634
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 18:03:28 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Columns'] = 'עמודות';
+$lang['Edit shortcuts'] = 'עריכת קיצורי דרך';
+$lang['If you change current set you will lost every shortcuts changes.'] = 'אם תשנה את הסט הנוכחי תאבד כל שינויי הקיצורים.';
+$lang['Reset defaults'] = 'איפוס לברירות המחדל';
+$lang['Shortcuts'] = 'קיצורי דרך';
+$lang['Smilies'] = 'רמזורים';
+$lang['Smilies set'] = 'סט רמזורים';
+$lang['Type in a new shortcut'] = 'הקלד קיצור דרך חדש';
